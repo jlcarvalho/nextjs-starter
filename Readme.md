614a80fa219fa16f88d1a883ef7cd88eb7d8e7f6
Jean Lucas de Carvalho <jeanlucasdecarvalho@gmail.com>
2017-01-13 12:39:46 -0200

    First commit

---
+++ Readme.md
@@ -0,0 +1,23 @@
+# Next.js Starter
+
+This project uses [Next.js](https://github.com/zeit/next.js), Jest, Enzyme, Babel and Yarn.
+
+## Installation
+
+    $ yarn install
+
+## Running the app
+
+    $ yarn run dev
+
+## Thest the app
+
+    $ yarn test
+
+### Genereate test snapshot
+
+    $ yarn test -- -u
+
+## Running in production
+
+    $ yarn start
---
+++ __tests__/index.test.js
@@ -0,0 +1,27 @@
+/* global it, expect, describe */
+
+import React from 'react'
+import { shallow } from 'enzyme'
+import renderer from 'react-test-renderer'
+import App from '../pages/index.js'
+
+const photos = new Array(15).fill(0).map((v, k) => k + 1);
+const url = { pathname: '/', query: {} };
+
+describe('With Enzyme', () => {
+  it('App have 15 photos', () => {
+    const app = shallow(
+      <App photos={photos} url={url}/>
+    )
+
+    expect(app.find('.photo').length).toBe(15);
+  })
+})
+
+describe('With Snapshot Testing', () => {
+  it('App have 15 photos', () => {
+    const component = renderer.create(<App photos={photos} url={url}/>)
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
