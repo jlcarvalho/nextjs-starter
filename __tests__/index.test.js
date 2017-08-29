d5171413f4327af04b607c16bed0df44c937aaec
Jean Lucas de Carvalho <jeanlucasdecarvalho@gmail.com>
2017-08-29 13:12:05 -0300

    Run linter and tests on git pre-commit hook

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,22 +5,22 @@ import { shallow } from 'enzyme'
 import renderer from 'react-test-renderer'
 import App from '../pages/index.js'
 
-const photos = new Array(15).fill(0).map((v, k) => k + 1);
-const url = { pathname: '/', query: {} };
+const photos = new Array(15).fill(0).map((v, k) => k + 1)
+const url = { pathname: '/', query: {} }
 
 describe('With Enzyme', () => {
   it('App have 15 photos', () => {
     const app = shallow(
-      <App photos={photos} url={url}/>
+      <App photos={photos} url={url} />
     )
 
-    expect(app.find('.photo').length).toBe(15);
+    expect(app.find('.photo').length).toBe(15)
   })
 })
 
 describe('With Snapshot Testing', () => {
   it('App have 15 photos', () => {
-    const component = renderer.create(<App photos={photos} url={url}/>)
+    const component = renderer.create(<App photos={photos} url={url} />)
     const tree = component.toJSON()
     expect(tree).toMatchSnapshot()
   })
